deb626f306c6421e4a6cfdd8d974327d566ad10f
nikhil kumar <nikhilkumar2092@gmail.com>
2020-05-19 00:28:36 +0530

    Configured for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c605fnsmg@5wjoedgv02hx8f@a&=d7ii#4un=1jx3j6kzodi86'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = ['192.168.1.15', 'localhost', '127.0.0.1']
 
@@ -117,3 +117,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
